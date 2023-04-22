0ae0f54e703adc271f92c9e8cb4ee194bb156aa7
Paula Caires <62515804+paulacaires@users.noreply.github.com>
2023-04-22 10:00:34 -0300

    Sistema de notas escolares
    Esse exercício vai trabalhar a transformação de notas escolares, onde devemos criar um algoritmo para transformar notas númericas em notas de caracteres.
    
    Desafio:
    
    Crie um algoritmo que transforme as notas do sistema
    numérico para sistema de notas em caracteres tipo A B C
    
    * de 90 para cima -   A
    * entre 80 - 89   -   B
    * entre 70 - 79   -   C
    * entre 60 - 69   -   D
    * menor que 60    -   F

--- notas.js
+++ notas.js
@@ -1,29 +1,40 @@
 /*	Crie um algoritmo que transforme as notas do sistema
-numérico para sistema de notas em caracteres tipo A B C
+	numérico para sistema de notas em caracteres tipo A B C
 
-* de 90 para cima -   A
-* entre 80 - 89   -   B
-* entre 70 - 79   -   C
-* entre 60 - 69   -   D
-* menor que 60    -   F	*/
+	de 90 para cima -   A
+	entre 80 - 89   -   B
+	entre 70 - 79   -   C
+	entre 60 - 69   -   D
+	menor que 60    -   F	*/
 
-function notasChar (nota) {
+function converte_nota (nota) {
 	
-	if (nota >= 90)
-		return 'A';
+	if (nota >= 90 && nota <= 100)
+		return('A');
 	
 	if (nota >= 80 && nota <= 89)
-		return 'B';
+		return('B');
 		
 	if (nota >= 70 && nota <= 79)
-		return 'C';
+		return('C');
 		
 	if (nota >= 60 && nota <= 69)
-		return 'D';
+		return('D');
 		
-	return 'F';	
+	if (nota <= 60 && nota >= 0)
+		return('F');
+		
+	else
+		throw 'Nota inválida';	
 }
- 
-let nota = 85;
-console.log(notasChar(nota));
-console.log(nota <= 80);
+
+// Array com as notas que serão testadas pela função.
+const notas_teste = new Array(110, 90, 85, 50, 0, -1, 'A', 'Noventa', '70');
+
+for (let nota of notas_teste) {	
+	try {
+		console.log(converte_nota(nota));
+	} catch(e) {
+		console.log(`A nota ${nota} é inválida`);
+	}
+}
